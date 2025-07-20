80894d9479282f3a213a78a5d08b380aabe9c317
sumanengg.sg <sumanengg.sg@gmail.com>
2025-07-20 23:04:58 +0530

    Update code

--- mygit/cli.py
+++ mygit/cli.py
@@ -1,12 +1,20 @@
 import argparse
-from . import data
-import os
+from . import data, base
+import os, sys
 
 def main():
     parser = argparse.ArgumentParser()
     commands = parser.add_subparsers()
     init_parser = commands.add_parser("init")
     init_parser.set_defaults(func=init)
+    hash_object_parcer = commands.add_parser("hash-object")
+    hash_object_parcer.add_argument("file")
+    hash_object_parcer.set_defaults(func=hash_function)
+    # cat-file to get the object 
+    cat_file_parcer = commands.add_parser("cat-file")
+    cat_file_parcer.add_argument("oid")
+    cat_file_parcer.set_defaults(func=cat_file)
+
     args = parser.parse_args()
 
     if hasattr(args, "func"):
@@ -17,6 +25,24 @@ def main():
 def init(args):
     data.init()
     print (f'Initialized empty ugit repository in {os.getcwd()}/{data.GIT_DIR}')
+
+# Hash_function to take file as argument and stroed it as content-addressble
+
+def hash_function(args):
+    try:
+        with open(args.file, 'rb') as f:
+            data.hash_object(f.read())
+    except Exception as e:
+        raise
+
+# get_object function is used to get the object using oid of the file, loaded earlier
+
+def cat_file(args):
+    try:
+        sys.stdout.flush()
+        sys.stdout.buffer.write(data.get_object(args.oid))
+    except Exception as e:
+        raise
 
 if __name__ == "__main__":
     main()
--- mygit/data.py
+++ mygit/data.py
@@ -1,9 +1,32 @@
 import os
+import hashlib
 
 GIT_DIR = ".mygit"
 
 def init():
     try:
-        os.makedirs(GIT_DIR)
+        os.makedirs(GIT_DIR, exist_ok=True)
+        os.makedirs(f'{GIT_DIR}/objects', exist_ok=True)
     except Exception as e:
-        pass
+        raise
+
+def hash_object(data, obj_type="blob"):
+    try:
+        header = f"{obj_type}\0".encode()
+        full_data = header + data
+        oid = hashlib.sha1(full_data).hexdigest()
+        with open(f'{GIT_DIR}/objects/{oid}', 'wb') as f:
+            f.write(full_data)
+        return oid
+    except Exception as e:
+        raise
+
+# get_object now returns (type, data)
+def get_object(oid):
+    try:
+        with open(f'{GIT_DIR}/objects/{oid}', 'rb') as f:
+            full_data = f.read()
+        obj_type, _, data = full_data.partition(b'\0')
+        return data
+    except Exception as e:
+        raise
